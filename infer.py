3e6994dda6e9bbbc4c98ed946ce59003f48be567
amsozzer1 <Asozzer2@illinois.edu>
2026-08-16 17:24:37 -0400

    inference updated

--- infer.py
+++ infer.py
@@ -3,33 +3,30 @@
 # Retrieved 2026-08-16, License - CC BY-SA 4.0
 
 import json
-import random
 
 import numpy as np
 
 from matrix import matrix
 from mnist_loader import load_mnist
 from typedefs import Layer
-from utils import classification, feature_extraction, he_kernel
+from utils import classification, feature_extraction
 
-random.seed(0)
-conv1_filters = [[he_kernel(3, 1)] for _ in range(32)]                      # 32 filters, 1 channel each
-conv2_filters = [[he_kernel(3, 32) for _ in range(32)] for _ in range(64)]
 with open('weights/epoch3.json') as f:
     d = json.load(f)
     shapes: tuple[int,int] = d['shapes']
     weights: list[matrix] = [matrix(i) for i in d["weights"]]
     biases: list[list[float]] = d['biases']
+    kernels: list[list[list[matrix]]] = [[[matrix(k) for k in stack] for stack in layer]
+                                         for layer in d["kernels"]]
     correct = 0
-    imgs,lbls = load_mnist("test", limit=100)
+    imgs,lbls = load_mnist("test", limit=1000)
+    layers: list[Layer] = [
+        (kernels[0], 1),
+        (kernels[1], 1),
+    ]
     for l in range(len(imgs)):
         img, lbl = imgs[l], lbls[l]
         input = matrix([[p for p in row] for row in img])
-        layers: list[Layer] = [
-            (conv1_filters,1),
-            (conv2_filters,1),
-        ]
-
         features, _ = feature_extraction(layers, [input]) # dump=str(n) stores images per layer # FEATURE EXTRACTION
         probs, probs_caches = classification(features,weights[0],biases[0],weights[1],biases[1]) # CLASSIFICATION
         curr = - np.inf,0
@@ -42,4 +39,4 @@ with open('weights/epoch3.json') as f:
             correct+=1
         confidence = curr[0] * 100
         print(prediction, lbl, confidence)
-    print(correct, 100, str(correct)+"%")
+    print(correct, len(imgs), f"{100 * correct / len(imgs):.1f}%")
